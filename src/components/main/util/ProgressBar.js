c6a5d6210595d4c20ccc2d7036a6a0b158471b4f
towm1204 <17towm@gmail.com>
2020-08-13 19:12:53 -0700

    fixed progress bar)

--- src/components/main/util/ProgressBar.js
+++ src/components/main/util/ProgressBar.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react';
 
 export default class ProgressBar extends Component{
   render() {
+    let percent = Math.min((this.props.balance / this.props.total * 100), 100)
     let progressStyle = {
       width: (this.props.balance / this.props.total * 100) + "%"
     };
+    if (percent >= 100) {
+      progressStyle["background-color"] = "#ff0000"
+    }
     return (
       <div className="progressBar">
         <div className="progress" style={progressStyle}></div>

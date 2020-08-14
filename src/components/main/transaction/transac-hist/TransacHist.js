b4c24a04ec1faa5de88ab4d09f0ce153749b477e
towm1204 <17towm@gmail.com>
2020-08-13 18:08:48 -0700

    fixed history overflow

--- src/components/main/transaction/transac-hist/TransacHist.js
+++ src/components/main/transaction/transac-hist/TransacHist.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
 import TransacHistItem from './TransacHistItem';
 
+class TransacListItem extends Component {
+  render() {
+    return(
+      <li class="list-group-item">
+        {this.props.row.description + ":     "+ this.props.row.amount + "$"}
+      </li>);
+  }
+}
+
 export default class TransacHist extends Component {
   render() {
     let sortedHist =[]
     if (this.props.hist) {
       sortedHist = this.props.hist.sort((a, b) => a.dateTime - b.dateTime);
     }
+    let listRows = sortedHist.map((hist) => {
+      return(<TransacListItem key ={hist.dateTime} row={hist}/>)
+    })
+
+
     let rows = sortedHist.map((hist) => {
       return(<TransacHistItem key={hist.dateTime} row={hist}/>)
     })
+
+
     return (
       <div className="modal fade" id="book" tabIndex="-1" role="dialog">
         <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
@@ -18,21 +34,27 @@ export default class TransacHist extends Component {
               <h5 className="modal-title">Transaction History</h5>
             </div>
             <div className="modal-body">
-            <table className="table table-hover">
-              <thead className="thead">
-                <tr>
-                  <th scope="col">Timestamp</th>
-                  <th scope="col">Description</th>
-                  <th scope="col">Account</th>
-                  <th scope="col">For</th>
-                  <th scope="col">+/-</th>
-                  <th scope="col">Total Balance</th>
-                </tr>
-              </thead>
-              <tbody>
-                {rows}
-              </tbody>
-            </table>
+
+              <table className="table table-hover" id="transactionTable">
+                <thead className="thead">
+                  <tr>
+                    <th scope="col">Timestamp</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Account</th>
+                    <th scope="col">For</th>
+                    <th scope="col">+/-</th>
+                    <th scope="col">Total Balance</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {rows}
+                </tbody>
+              </table>
+
+              <ul class="list-group" id="transactionList">
+                {listRows}
+              </ul>
+
             </div>
             <div className="modal-footer">
               <button type="button" aria-label= "CloseBtn" className="btn btn-secondary" data-dismiss="modal" htmlFor="transacClosed">Close</button>
